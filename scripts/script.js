cd18daf1fcd22ec9f52cccae302e330865f07ed1
Adrian Pappalardo <adrian_xdrag@hotmail.com>
2015-12-04 12:13:51 +0100

    Home with subcategories
    Basic javascript
    Divs not hidden

--- scripts/script.js
+++ scripts/script.js
@@ -1,150 +1,165 @@
 $(document).ready(function () {
 	$("#c1").click(function () {
-		$("#m1").slideToggle("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideToggle("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c2").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideToggle("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideToggle("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c3").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideToggle("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideToggle("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c4").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideToggle("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideToggle("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c5").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideToggle("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideToggle("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c6").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideToggle("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideToggle("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c7").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideToggle("fast", function () {
-		});
-		$("#m8").slideUp("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideToggle("fast", function () {});
+		$("#m8").slideUp("fast", function () {});
 	});
 	$("#c8").click(function () {
-		$("#m1").slideUp("fast", function () {
-		});
-		$("#m2").slideUp("fast", function () {
-		});
-		$("#m3").slideUp("fast", function () {
-		});
-		$("#m4").slideUp("fast", function () {
-		});
-		$("#m5").slideUp("fast", function () {
-		});
-		$("#m6").slideUp("fast", function () {
-		});
-		$("#m7").slideUp("fast", function () {
-		});
-		$("#m8").slideToggle("fast", function () {
-		});
+		$("#m1").slideUp("fast", function () {});
+		$("#m2").slideUp("fast", function () {});
+		$("#m3").slideUp("fast", function () {});
+		$("#m4").slideUp("fast", function () {});
+		$("#m5").slideUp("fast", function () {});
+		$("#m6").slideUp("fast", function () {});
+		$("#m7").slideUp("fast", function () {});
+		$("#m8").slideToggle("fast", function () {});
 	});
-    $("#contact").click(function () {
-		$("#contactopt").slideToggle("fast", function () {
-		});
+	$("#contact").click(function () {
+		$("#contactopt").slideToggle("fast", function () {});
+	});
+	$("#main_cat1").click(function () {
+		$("#cat1").slideToggle("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat2").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideToggle("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat3").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideToggle("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat4").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideToggle("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});;
+	});
+	$("#main_cat5").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideToggle("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat6").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideToggle("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat7").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideToggle("fast", function () {});
+		$("#cat8").slideUp("fast", function () {});
+	});
+	$("#main_cat8").click(function () {
+		$("#cat1").slideUp("fast", function () {});
+		$("#cat2").slideUp("fast", function () {});
+		$("#cat3").slideUp("fast", function () {});
+		$("#cat4").slideUp("fast", function () {});
+		$("#cat5").slideUp("fast", function () {});
+		$("#cat6").slideUp("fast", function () {});
+		$("#cat7").slideUp("fast", function () {});
+		$("#cat8").slideToggle("fast", function () {});
 	});
 });
