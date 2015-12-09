f77aef1b320f8ff58d99158d0d0e5e29a0594b25
Daniel Fernandez Rico <deferrico@gmail.com>
2015-12-09 11:39:34 +0100

    Dynamic footer added.
    Test 1: scroll down = disappear.

--- scripts/script.js
+++ scripts/script.js
@@ -87,6 +87,42 @@ $(document).ready(function () {
 	$("#main_cat9").click(function () {
 		location.href = "subcategory.html";
 	});
+	
+		// FOOTER SHIT
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $("#footer").outerHeight();
+
+	$(window).scroll(function(event){
+		didScroll = true;
+	});
+
+	setInterval(function() {
+		if (didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+	}, 250);
+
+	function hasScrolled() {
+		var st = $(this).scrollTop();
+
+		// Make sure they scroll more than delta
+		if(Math.abs(lastScrollTop - st) <= delta)
+			return;
+		
+		if (st > lastScrollTop && st > navbarHeight){
+			// Scroll Down
+			$('#footer').removeClass('nav-down').addClass('nav-up');
+		} else {
+			// Scroll Up
+			if(st + $(window).height() < $(document).height()) {
+				$('#footer').removeClass('nav-up').addClass('nav-down');
+			}
+		}
+		lastScrollTop = st;
+	}
 });
 
 
