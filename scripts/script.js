c1c1770619711cfe1c3565865ae7a69f982644e0
Daniel Fernandez Rico <deferrico@gmail.com>
2015-12-10 15:24:38 +0100

    Script cleaning.
    Previous footer removed.

--- scripts/script.js
+++ scripts/script.js
@@ -87,7 +87,7 @@ $(document).ready(function () {
 		location.href = "subcategory.html";
 	});
     
-    // FOOTER SHIT
+    // FOOTER TOGGLE
     var height;
     var trigger = 50;
     $(window).scroll(function() {
@@ -98,47 +98,6 @@ $(document).ready(function () {
     		$('#footer').slideUp(300);
     	}
     });
-    
-	/*
-    var didScroll;
-	var lastScrollTop = 0;
-	var delta = 5;
-	var navbarHeight = $("#footer").outerHeight();
-
-	$(window).scroll(function(event){
-		didScroll = true;
-	});
-
-	setInterval(function() {
-		if (didScroll) {
-			hasScrolled();
-			didScroll = false;
-		}
-	}, 250);
-
-	function hasScrolled() {
-		var st = $(this).scrollTop();
-
-		// Make sure they scroll more than delta
-		if(Math.abs(lastScrollTop - st) <= delta)
-			return;
-		
-		if (st > lastScrollTop && st > navbarHeight){
-			// Scroll Down
-			$('#footer').removeClass('nav-up').addClass('nav-down');
-		} else {
-			// Scroll Up
-			if(st + $(window).height() < $(document).height()) {
-				$('#footer').removeClass('nav-down').addClass('nav-up');
-			}
-		}
-		lastScrollTop = st;
-	}
-	
-	if ($("body").height() <= $(window).height()) {
-        $('#footer').removeClass('nav-up').addClass('nav-down');
-    }
-    */
 });
 
 
